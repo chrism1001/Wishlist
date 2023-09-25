b275e987bb5ee7924b9dbe2ea16f8adcc319472b
Christopher Mejia <58187990+chrism1001@users.noreply.github.com>
2023-09-25 14:23:55 -0400

    Update mainactivity file to read input when button is pressed and display with adapter

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -2,11 +2,35 @@ package com.example.wishlist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var items = List<Item>
+    private lateinit var items: MutableList<Item>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val itemsRv = findViewById<RecyclerView>(R.id.itemsRv)
+        val button = findViewById<Button>(R.id.submitButton)
+        val nameInput = findViewById<TextInputEditText>(R.id.itemNameInput)
+        val priceInput = findViewById<TextInputEditText>(R.id.priceInput)
+        val websiteInput = findViewById<TextInputEditText>(R.id.itemWebsiteInput)
+
+        val adapter = WishAdapter(items)
+        itemsRv.adapter = adapter
+        itemsRv.layoutManager = LinearLayoutManager(this)
+
+        button.setOnClickListener {
+            val newItem = Item(nameInput.toString(),
+                priceInput.toString(), websiteInput.toString())
+            items.add(newItem)
+            nameInput.text?.clear()
+            priceInput.text?.clear()
+            websiteInput.text?.clear()
+            adapter.notifyDataSetChanged()
+        }
     }
 }
