6372eeaf7d21f8b11e2ef1517333ac5f9aac1e95
Christopher Mejia <58187990+chrism1001@users.noreply.github.com>
2023-09-25 13:47:57 -0400

    Update wishlist adapter class and implement related member functions

--- app/src/main/java/com/example/wishlist/WishAdapter.kt
+++ app/src/main/java/com/example/wishlist/WishAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.wishlist
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class WishAdapter {
+class WishAdapter(private val items: List<Item>) : RecyclerView.Adapter<WishAdapter.ViewHolder>() {
     // Provide a direct reference to each of the views within a data item
     // Used to cache the views within the item layout for fast access
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -17,6 +19,24 @@ class WishAdapter {
             priceTextView = itemView.findViewById(R.id.priceTv)
             websiteTextView = itemView.findViewById(R.id.websiteTv)
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        val contactView = inflater.inflate(R.layout.item_layout, parent, false)
+        return ViewHolder(contactView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items.get(position)
+        holder.nameTextView.text = item.itemName
+        holder.priceTextView.text = item.itemPrice
+        holder.websiteTextView.text = item.website
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
     }
 }
 
